82d5cd8eb1c57351f47e22c361440221199c636c
Harsh-09A <harsh.autowebbed@gmail.com>
2025-09-16 13:11:14 +0530

    Home Page Done

---
+++ includes/footer.php
@@ -0,0 +1,149 @@
+    <!-- start footer -->
+    <footer
+      class="bg-black pb-4 sm-pb-50px"
+      style="background-image: url(img/footer/footer_bg_pattern.svg)">
+      <div class="container">
+        <div class="row mb-6">
+          <div class="col-lg-5 col-md-6 sm-mb-30px order-2 order-md-1">
+            <h3
+              class="text-white fw-500 alt-font mb-50px ls-minus-1px sm-mb-30px">
+              Let's make something great work together.
+            </h3>
+            <div class="row">
+              <div class="col-lg-5 col-6 footer_text-white">
+                <span
+                  class="alt-font fs-14 text-uppercase d-block text-base-color ls-1px lh-24"
+                  >Call us</span
+                >
+                <a href="tel:+91 90299 29777">+91 90299 29777</a>
+              </div>
+              <div class="col-lg-6 col-6 footer_text-white">
+                <span
+                  class="alt-font fs-14 text-uppercase d-block text-base-color ls-1px lh-24"
+                  >Email Us</span
+                >
+                <a href="mailto:info@propadvisor.co.in"
+                  >info@propadvisor.co.in</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2 offset-lg-1 col-6 order-3 order-md-2">
+            <span
+              class="alt-font fs-14 text-uppercase mb-5px d-block text-base-color ls-1px"
+              >Projects</span
+            >
+            <ul class="footer_text-white">
+              <li><a href="">Delta Prestige </a></li>
+              <li><a href="">Invicta Ulwe</a></li>
+              <li><a href="">Invicta EClat</a></li>
+              <li>
+                <a href="">Delta Greens</a>
+              </li>
+              <li><a href="">Delta Elite</a></li>
+            </ul>
+          </div>
+          <div class="col-md-2 col-6 order-3 order-md-3">
+            <span
+              class="alt-font fs-14 text-uppercase mb-5px d-block text-base-color ls-1px"
+              >information</span
+            >
+            <ul class="footer_text-white">
+              <li>
+                <a href="" target="_blank">Blogs</a>
+              </li>
+              <li>
+                <a href="" target="_blank">Career</a>
+              </li>
+              <li>
+                <a href="" target="_blank">Privacy Policy </a>
+              </li>
+              <li>
+                <a href="" target="_blank">Disclaimer </a>
+              </li>
+              <li>
+                <a href="" target="_blank">Terms & Condition</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-2 order-1 order-md-4 sm-mb-30px">
+            <div
+              class="col-lg-12 col-md-12 col-sm-12 elements-social social-icon-style-10 text-white">
+              <span
+                class="alt-font fs-14 text-uppercase mb-5px d-block text-base-color ls-1px"
+                >Follow Us</span
+              >
+              <ul class="">
+                <li class="me-30px">
+                  <a
+                    class="facebook"
+                    href="https://www.facebook.com/"
+                    target="_blank">
+                    <i class="fa-brands fa-facebook-f me-10px"></i>
+                    <span class="fw-500">Facebook</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="instagram"
+                    href="http://www.instagram.com"
+                    target="_blank">
+                    <i class="fa-brands fa-instagram me-10px"></i>
+                    <span class="fw-500">Instagram</span>
+                  </a>
+                </li>
+
+                <li class="me-30px">
+                  <a
+                    class="youtube"
+                    href="http://www.youtube.com"
+                    target="_blank">
+                    <i class="fa-brands fa-youtube me-10px"></i>
+                    <span class="fw-500">Youtube</span>
+                  </a>
+                </li>
+                <li class="me-30px">
+                  <a
+                    class="youtube"
+                    href="http://www.youtube.com"
+                    target="_blank">
+                    <i class="fa-brands fa-linkedin me-10px"></i>
+                    <span class="fw-500">Linkedin</span>
+                  </a>
+                </li>
+                <li class="me-30px">
+                  <a
+                    class="twitter"
+                    href="http://www.twitter.com"
+                    target="_blank">
+                    <i class="fa-brands fa-x-twitter me-10px"></i>
+                    <span class="fw-500">X &nbsp;&nbsp;&nbsp;</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-7 last-paragraph-no-margin">
+            <p class="fs-13 lh-22 w-95 sm-w-100">
+              This site is protected by reCAPTCHA and the Google privacy policy
+              and terms of service apply. You must not use this website if you
+              disagree with any of these website standard terms and conditions.
+            </p>
+          </div>
+          <div class="col-md-5 text-md-end sm-mt-15px last-paragraph-no-margin">
+            <p class="fs-13 lh-22">
+              &copy; 2025 Powered by
+              <a
+                href="https://letsdigin.in/"
+                target="_blank"
+                class="text-decoration-line-bottom text-base-color"
+                >DIGIN Media</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- end footer -->
--- index.php
+++ index.php
@@ -350,7 +350,7 @@
     </section>
     <!-- end about section -->
 
-    <!-- start about section -->
+    <!-- start journey heading section -->
     <section class="pt-50px">
       <div class="container text-center">
         <div class="mb-10px">
@@ -366,9 +366,9 @@
         </h4>
       </div>
     </section>
-    <!-- end about section -->
+    <!-- end journey heading section -->
 
-    <!-- start section -->
+    <!-- start journey section -->
     <section
       class="pt-0 border-top border-color-extra-medium-gray mt-0 md-mt-0 sm-border-top-0">
       <div class="container">
@@ -446,9 +446,9 @@
         </div>
       </div>
     </section>
+    <!-- end journey section -->
 
-    <!-- end section -->
-    <!-- start section -->
+    <!-- start marquee text section -->
     <section class="overflow-hidden position-relative pt-0 lg-pb-0">
       <div class="container-fluid">
         <div class="row position-relative">
@@ -484,8 +484,9 @@
         </div>
       </div>
     </section>
-    <!-- end section -->
-    <!-- start section -->
+    <!-- end marquee text section -->
+
+    <!-- start impact section -->
     <section>
       <div class="container">
         <div class="row justify-content-center align-items-center sm-pb-9">
@@ -594,9 +595,9 @@
         </div>
       </div>
     </section>
-    <!-- end section -->
+    <!-- end  impact section -->
 
-    <!-- start section -->
+    <!-- start experience section -->
     <section class="overflow-hidden">
       <div class="container">
         <div class="row mb-6 sm-mb-50px">
@@ -722,9 +723,9 @@
         </div>
       </div>
     </section>
-    <!-- end section -->
+    <!-- end experience section -->
 
-    <!-- start section -->
+    <!-- start featured projects section -->
     <section class="stack-box py-0 z-index-99">
       <div class="stack-box-contain">
         <!-- start stack item -->
@@ -926,10 +927,9 @@
         <!-- end stack item -->
       </div>
     </section>
-    <!-- end section -->
-    <!-- section removed -->
+    <!-- end featured projects section -->
 
-    <!-- start section -->
+    <!-- start testimonial section -->
     <section class="big-section position-relative pb-0 mt-70px">
       <div
         class="background-position-center-top background-no-repeat position-absolute h-100 w-100 left-0px top-0px"
@@ -1059,9 +1059,9 @@
         </div>
       </div>
     </section>
-    <!-- end section -->
+    <!-- end testimonial section -->
 
-    <!-- start about section -->
+    <!-- start clients logo title section -->
     <section class="py-0">
       <div class="container text-center">
         <div class="mb-10px">
@@ -1077,8 +1077,9 @@
         </h4>
       </div>
     </section>
-    <!-- end about section -->
-    <!-- start section -->
+    <!-- end clients logo title section -->
+
+    <!-- start clients logo section -->
     <section class="position-relative overflow-hidden py-0 pb-70px">
       <div class="bg-overlay-light"></div>
 
@@ -1496,9 +1497,9 @@
         </div>
       </div>
     </section>
-    <!-- end section -->
+    <!-- end clients logo section -->
 
-    <!-- start section -->
+    <!-- start cta section -->
     <section
       data-parallax-background-ratio="0.5"
       style="background-image: url('img/cta/cta_bg_sky_scapper.jpg')">
@@ -1532,157 +1533,10 @@
         </div>
       </div>
     </section>
-    <!-- end section -->
+    <!-- end cta section -->
 
-    <!-- start footer -->
-    <footer
-      class="bg-black pb-4 sm-pb-50px"
-      style="background-image: url(img/footer/footer_bg_pattern.svg)">
-      <div class="container">
-        <div class="row mb-6">
-          <div class="col-lg-5 col-md-6 sm-mb-30px order-2 order-md-1">
-            <h3
-              class="text-white fw-500 alt-font mb-50px ls-minus-1px sm-mb-30px">
-              Let's make something great work together.
-            </h3>
-            <div class="row">
-              <div class="col-lg-5 col-6 footer_text-white">
-                <span
-                  class="alt-font fs-14 text-uppercase d-block text-base-color ls-1px lh-24"
-                  >Call us</span
-                >
-                <a href="tel:+91 90299 29777">+91 90299 29777</a>
-              </div>
-              <div class="col-lg-6 col-6 footer_text-white">
-                <span
-                  class="alt-font fs-14 text-uppercase d-block text-base-color ls-1px lh-24"
-                  >Email Us</span
-                >
-                <a href="mailto:info@propadvisor.co.in"
-                  >info@propadvisor.co.in</a
-                >
-              </div>
-            </div>
-          </div>
-          <div class="col-md-2 offset-lg-1 col-6 order-3 order-md-2">
-            <span
-              class="alt-font fs-14 text-uppercase mb-5px d-block text-base-color ls-1px"
-              >Projects</span
-            >
-            <ul class="footer_text-white">
-              <li><a href="">Delta Prestige </a></li>
-              <li><a href="">Invicta Ulwe</a></li>
-              <li><a href="">Invicta EClat</a></li>
-              <li>
-                <a href="">Delta Greens</a>
-              </li>
-              <li><a href="">Delta Elite</a></li>
-            </ul>
-          </div>
-          <div class="col-md-2 col-6 order-3 order-md-3">
-            <span
-              class="alt-font fs-14 text-uppercase mb-5px d-block text-base-color ls-1px"
-              >information</span
-            >
-            <ul class="footer_text-white">
-              <li>
-                <a href="" target="_blank">Blogs</a>
-              </li>
-              <li>
-                <a href="" target="_blank">Career</a>
-              </li>
-              <li>
-                <a href="" target="_blank">Privacy Policy </a>
-              </li>
-              <li>
-                <a href="" target="_blank">Disclaimer </a>
-              </li>
-              <li>
-                <a href="" target="_blank">Terms & Condition</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-2 order-1 order-md-4 sm-mb-30px">
-            <div
-              class="col-lg-12 col-md-12 col-sm-12 elements-social social-icon-style-10 text-white">
-              <span
-                class="alt-font fs-14 text-uppercase mb-5px d-block text-base-color ls-1px"
-                >Follow Us</span
-              >
-              <ul class="">
-                <li class="me-30px">
-                  <a
-                    class="facebook"
-                    href="https://www.facebook.com/"
-                    target="_blank">
-                    <i class="fa-brands fa-facebook-f me-10px"></i>
-                    <span class="fw-500">Facebook</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    class="instagram"
-                    href="http://www.instagram.com"
-                    target="_blank">
-                    <i class="fa-brands fa-instagram me-10px"></i>
-                    <span class="fw-500">Instagram</span>
-                  </a>
-                </li>
+    <?php include_once "./includes/footer.php" ?>
 
-                <li class="me-30px">
-                  <a
-                    class="youtube"
-                    href="http://www.youtube.com"
-                    target="_blank">
-                    <i class="fa-brands fa-youtube me-10px"></i>
-                    <span class="fw-500">Youtube</span>
-                  </a>
-                </li>
-                <li class="me-30px">
-                  <a
-                    class="youtube"
-                    href="http://www.youtube.com"
-                    target="_blank">
-                    <i class="fa-brands fa-linkedin me-10px"></i>
-                    <span class="fw-500">Linkedin</span>
-                  </a>
-                </li>
-                <li class="me-30px">
-                  <a
-                    class="twitter"
-                    href="http://www.twitter.com"
-                    target="_blank">
-                    <i class="fa-brands fa-x-twitter me-10px"></i>
-                    <span class="fw-500">X &nbsp;&nbsp;&nbsp;</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-7 last-paragraph-no-margin">
-            <p class="fs-13 lh-22 w-95 sm-w-100">
-              This site is protected by reCAPTCHA and the Google privacy policy
-              and terms of service apply. You must not use this website if you
-              disagree with any of these website standard terms and conditions.
-            </p>
-          </div>
-          <div class="col-md-5 text-md-end sm-mt-15px last-paragraph-no-margin">
-            <p class="fs-13 lh-22">
-              &copy; 2025 Powered by
-              <a
-                href="https://letsdigin.in/"
-                target="_blank"
-                class="text-decoration-line-bottom text-base-color"
-                >DIGIN Media</a
-              >
-            </p>
-          </div>
-        </div>
-      </div>
-    </footer>
-    <!-- end footer -->
     <!-- start scroll progress -->
     <div class="scroll-progress d-none d-xxl-block">
       <a href="#" class="scroll-top" aria-label="scroll">
@@ -1691,6 +1545,7 @@
       </a>
     </div>
     <!-- end scroll progress -->
+
     <!-- javascript libraries -->
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/vendors.min.js"></script>
